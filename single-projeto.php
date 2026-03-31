b2f2321c9fde8d062a9049edf452f03bfa0f2317
Rafael Bandeira <rafabandeira@gmail.com>
2026-03-31 08:57:54 -0300

    v1.0.5

--- single-projeto.php
+++ single-projeto.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template para exibição de um Projeto Individual
- * Fidelidade Total à imagem image_e7d11d.jpg
+ * Correção de proporção da imagem e layout de texto em largura total
  */
 
 get_header(); ?>
@@ -11,15 +11,17 @@ get_header(); ?>
     <div class="breadcrumbs d-flex align-items-center"
         style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/breadcrumbs-bg.jpg');">
         <div class="container position-relative d-flex flex-column align-items-center">
-            <h2>Detalhes do Projeto</h2>
+            <h2><?php the_title(); ?></h2>
             <ol>
-                <li><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></li>
-                <li>Detalhes do Projeto</li>
+                <li><a href="<?php echo home_url(); ?>">Home</a></li>
+                <li><a href="<?php echo get_post_type_archive_link('projeto'); ?>">Projetos</a></li>
+                <li><?php the_title(); ?></li>
             </ol>
         </div>
     </div>
+
     <section id="project-details" class="project-details">
-        <div class="container" data-aos="fade-up" data-aos-delay="100">
+        <div class="container">
 
             <?php if (have_posts()):
                 while (have_posts()):
@@ -36,12 +38,18 @@ get_header(); ?>
                                         foreach ($galeria as $imagem): ?>
                                             <div class="swiper-slide">
                                                 <img src="<?php echo esc_url($imagem['url']); ?>"
-                                                    alt="<?php echo esc_attr($imagem['alt']); ?>">
+                                                    alt="<?php echo esc_attr($imagem['alt']); ?>" class="img-fluid rounded"
+                                                    style="width: 100%; height: auto; object-fit: contain;">
                                             </div>
                                         <?php endforeach;
                                     else: ?>
                                         <div class="swiper-slide">
-                                            <?php the_post_thumbnail('full'); ?>
+                                            <?php if (has_post_thumbnail()):
+                                                the_post_thumbnail('full', [
+                                                    'class' => 'img-fluid rounded',
+                                                    'style' => 'width: 100%; height: auto;'
+                                                ]);
+                                            endif; ?>
                                         </div>
                                     <?php endif; ?>
                                 </div>
@@ -50,7 +58,6 @@ get_header(); ?>
                         </div>
 
                         <div class="col-lg-4">
-
                             <div class="portfolio-info">
                                 <h3>Informações do projeto</h3>
                                 <ul>
@@ -60,33 +67,40 @@ get_header(); ?>
                                     $data = get_field('projeto_data');
                                     $url = get_field('projeto_url');
                                     ?>
-                                    <li><strong>Categoria</strong>
-                                        <span><?php echo $categoria ? esc_html($categoria) : 'Construção Civil'; ?></span></li>
-                                    <li><strong>Cliente</strong>
-                                        <span><?php echo $cliente ? esc_html($cliente) : 'J Filho Engenharia'; ?></span></li>
-                                    <li><strong>Data do projeto</strong>
-                                        <span><?php echo $data ? esc_html($data) : get_the_date(); ?></span></li>
-                                    <li><strong>URL do projeto</strong> <a
-                                            href="<?php echo $url ? esc_url($url) : '#'; ?>"><?php echo $url ? esc_url($url) : 'Privado'; ?></a>
-                                    </li>
+                                    <li><strong>Categoria</strong> <span>
+                                            <?php echo $categoria ? esc_html($categoria) : 'Construção Civil'; ?>
+                                        </span></li>
+                                    <li><strong>Cliente</strong> <span>
+                                            <?php echo $cliente ? esc_html($cliente) : 'J Filho Engenharia'; ?>
+                                        </span></li>
+                                    <li><strong>Data do projeto</strong> <span>
+                                            <?php echo $data ? esc_html($data) : get_the_date(); ?>
+                                        </span></li>
+                                    <li><strong>URL do projeto</strong> <a href="<?php echo $url ? esc_url($url) : '#'; ?>">
+                                            <?php echo $url ? esc_url($url) : 'Privado'; ?>
+                                        </a></li>
                                     <li><a href="<?php echo $url ? esc_url($url) : '#'; ?>"
                                             class="btn-visit align-self-start">Visitar Site</a></li>
                                 </ul>
                             </div>
+                        </div>
 
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-lg-12">
                             <div class="portfolio-description">
-                                <h2><?php the_title(); ?></h2>
+                                <h2>Descrição Detalhada</h2>
                                 <div class="content">
                                     <?php the_content(); ?>
                                 </div>
                             </div>
-
                         </div>
-
                     </div>
 
                 <?php endwhile; endif; ?>
 
         </div>
     </section>
-</main><?php get_footer(); ?>
+</main>
+
+<?php get_footer(); ?>
